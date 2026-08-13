0c6ddedda2eea12812cda3970cefbf858acf23ee
ChirkovU <105751811+ChirkovU@users.noreply.github.com>
2026-08-13 15:32:43 +0000

    Add files via upload

--- pw.js
+++ pw.js
@@ -1,1 +1,73 @@
-window.ADMIN_BETA_CODES = [];
+window.ADMIN_BETA_CODES = [
+    {
+      "code": "RAD-UZF7CU",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-VSWCCJ",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-SEFQ2C",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-Z6NJFX",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-C9GE47",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-DT4FCL",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-HBBV9B",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-KS2UNR",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-535T2H",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    },
+    {
+      "code": "RAD-A5MARA",
+      "active": true,
+      "used": false,
+      "usedByDevice": "",
+      "createdAt": 1786634576011
+    }
+  ];
+  
