570a5f7cfebe348f27a29aa5acec244fc8eb1185
ChirkovU <105751811+ChirkovU@users.noreply.github.com>
2026-08-13 22:43:30 +0400

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const RADAR_DB = {
   regions: {
     // ===== Москва / Московская обл =====
     "москва": {
-      fullName: "город Москва",
+      fullName: "город Мосва",
       coords: [55.7558, 37.6173],
       zoomLevel: 8
     },
